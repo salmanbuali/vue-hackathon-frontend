8487725f88fcdbada47f02f93b561db8adda78bf
Hamad-Alfandi <Hamad.alfandi7@gmail.com>
2024-03-13 11:03:23 +0300

    75% compolete student details page

--- src/pages/StudentDetails.vue
+++ src/pages/StudentDetails.vue
@@ -13,12 +13,14 @@ export default {
     async getAllStudents() {
       const response = await axios.get(`http://localhost:3001/students`)
       this.student = response.data.filter(
-          (e) => e.studentId == parseInt(this.$route.params.student_id)
-        )[0]
+        (e) => e.studentId == parseInt(this.$route.params.student_id)
+      )[0]
+
+      console.log(this.student)
       // console.log(
-        // this.student.filter(
-        //   (e) => e.studentId == parseInt(this.$route.params.student_id)
-        // )[0].name
+      // this.student.filter(
+      //   (e) => e.studentId == parseInt(this.$route.params.student_id)
+      // )[0].name
       // )
     },
   },
@@ -33,6 +35,30 @@ export default {
     Student Name: {{ this.student.name }}
     <br />
     ID: {{ this.student.studentId }}
+    <br />
+    GPA: {{ this.student.gpa }}
+  </div>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Course</th>
+          <th>Grade</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="e in this.student.results">
+          <td>
+            <!-- {{ this.student.results }} -->
+            {{ e.course.name }}
+          </td>
+          <td>
+            {{ e.grade.letter }}
+          </td>
+        </tr>
+       
+      </tbody>
+    </table>
   </div>
 </template>
 
